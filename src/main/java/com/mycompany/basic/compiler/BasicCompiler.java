78a60891ffafcf59ceb464a72ec4e01d34b9226d
Joao Vitor batistella <joaovitor.batistella.ifrs@gmail.com>
2023-08-27 01:20:35 -0300

    version 1.0

---
+++ src/main/java/com/mycompany/basic/compiler/BasicCompiler.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basic.compiler;
+
+import com.mycompany.views.MainForm;
+
+/**
+ *
+ * @author joaovb
+ */
+public class BasicCompiler {
+
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
+    }
+}
